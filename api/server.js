69e717681f0de62bdb25091c8447d91716bae3cd
Md Asad Chowdhary <shilvasad@gmail.com>
2025-08-08 13:29:14 +0600

    Basic server creation is running. I'll go through the database configuration soon.

---
+++ api/server.js
@@ -0,0 +1,27 @@
+import dotenv from "dotenv";
+import express from "express";
+import cors from "cors";
+dotenv.config();
+const app = express();
+const PORT = process.env.PORT;
+const host = process.env.host;
+const protocol = process.env.protocol;
+
+// CORS middleware, allow all origins.
+app.use(cors());
+
+// Enable JSON body parsing.
+app.use(express.json());
+
+// Test route.
+app.get("/", (req, res) => {
+  res.send("API is running...");
+});
+
+// Starting server
+
+app.listen(PORT, () => {
+  console.log(`
+    Server is running on PORT ${PORT}.
+    Access the server on ${protocol}://${host}:${PORT}`);
+});
