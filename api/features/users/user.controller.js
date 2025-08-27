dd06cf1823130d8635eb46c2b3c707ee8be444cb
Md Asad Chowdhary <shilvasad@gmail.com>
2025-08-27 19:03:28 +0600

    User module updated. Only admin can update some informations of users and delete a user.

--- api/features/users/user.controller.js
+++ api/features/users/user.controller.js
@@ -1,7 +1,7 @@
 import asyncHandler from "express-async-handler";
 import User from "./user.model.js";
 import bcrypt from "bcryptjs";
-import generateToken from '../../utils/generateToken.js'
+import generateToken from "../../utils/generateToken.js";
 
 /* 
 
@@ -50,10 +50,6 @@ const registerUser = asyncHandler(async (req, res) => {
   }
 });
 
-
-
-
-
 /*
 @desc   Authenticate user & get token. 
 @route  POST /api/users/login
@@ -61,51 +57,46 @@ const registerUser = asyncHandler(async (req, res) => {
 
 */
 
-const loginUser = asyncHandler(async(req, res)=>{
-  const {phone, password}=req.body
+const loginUser = asyncHandler(async (req, res) => {
+  const { phone, password } = req.body;
 
-  // Find the user using phone 
-  const user = await  User.findOne({phone})
+  // Find the user using phone
+  const user = await User.findOne({ phone });
 
-  if(user &&(await bcrypt.compare(password, user.password))){
+  if (user && (await bcrypt.compare(password, user.password))) {
     res.json({
       _id: user._id,
       name: user.name,
       phone: user.phone,
       role: user.role,
-      token: generateToken(user._id)
-    })
+      token: generateToken(user._id),
+    });
+  } else {
+    res.status(401);
+    throw new Error("Invalid phone number or password.");
   }
-  else{
-    res.status(401)
-    throw new Error('Invalid phone number or password.')
-  }
-})
-
+});
 
 /* 
   @desc   Get user profile
   @route  GET /api/users/profile
   @access Private
-*/ 
+*/
 
-const getUserProfile = asyncHandler(async(req, res)=>{
-  const user = req.user
-  if(user){
+const getUserProfile = asyncHandler(async (req, res) => {
+  const user = req.user;
+  if (user) {
     res.json({
-      _id : user._id,
+      _id: user._id,
       name: user.name,
       phone: user.phone,
-      role: user.role 
-    })
+      role: user.role,
+    });
+  } else {
+    res.status(404);
+    throw new Error("User not found");
   }
-  else{
-    res.status(404)
-    throw new Error('User not found')
-  }
-})
-
-
+});
 
 /*
   @desc   Update user profile
@@ -113,32 +104,90 @@ const getUserProfile = asyncHandler(async(req, res)=>{
   @access Private
 */
 
-const updateUserProfile = asyncHandler(async(req, res)=>{
-  const user = await User.findById(req.user._id)
-  if(user){
+const updateUserProfile = asyncHandler(async (req, res) => {
+  const user = await User.findById(req.user._id);
+  if (user) {
     // If new name provided
-    user.name = req.body.name || user.name
+    user.name = req.body.name || user.name;
 
     // If new password is provided, hashed and update
-    if(req.body.password){
-      const salt = await bcrypt.genSalt(10)
-      user.password = await bcrypt.hash(req.body.password, salt)
+    if (req.body.password) {
+      const salt = await bcrypt.genSalt(10);
+      user.password = await bcrypt.hash(req.body.password, salt);
     }
-    const updatedUser = await user.save()
+    const updatedUser = await user.save();
     res.json({
       _id: updatedUser._id,
       name: updatedUser.name,
       phone: updatedUser.phone,
       role: updatedUser.role,
-      token: generateToken(updatedUser._id)
-    })
+      token: generateToken(updatedUser._id),
+    });
+  } else {
+    res.status(404);
+    throw new Error("User not found");
   }
-  else{
-    res.status(404)
-    throw new Error('User not found')
+});
+
+// =============== Admin Only ====================
+/* 
+  @desc   Get all users
+  @route  GET /api/users
+  @access Private/Admin
+*/
+const getUsers = asyncHandler(async (req, res) => {
+  const users = await User.find({});
+  res.json(users);
+});
+
+/* 
+  @desc   Delete a user
+  @route  DELETE /api/users/:id
+  @access Private/Admin
+*/
+
+const deleteUser = asyncHandler(async (req, res) => {
+  const user = await User.findById(req.params.id);
+  if (user) {
+    await user.deleteOne();
+    res.json({ message: "User removed" });
+  } else {
+    res.status(404);
+    throw new Error("User not found");
   }
+});
 
-})
+/*
+  @desc   Update a user
+  @route  PUT /api/users/:id
+  @access Private/Admin
+*/
+const updateUser = asyncHandler(async (req, res) => {
+  const user = await User.findById(req.params.id);
+  if (user) {
+    user.name = req.body.name || user.name;
+    user.phone = req.body.phone || user.phone;
+    user.role = req.body.role || user.role;
+    const updatedUser = await user.save();
 
+    res.json({
+      _id: updatedUser._id,
+      name: updatedUser.name,
+      phone: updatedUser.phone,
+      role: updatedUser.role,
+    });
+  } else {
+    res.status(404);
+    throw new Error("User not found");
+  }
+});
 
-export { registerUser, loginUser, getUserProfile , updateUserProfile};
+export {
+  registerUser,
+  loginUser,
+  getUserProfile,
+  updateUserProfile,
+  getUsers,
+  deleteUser,
+  updateUser,
+};
--- api/features/users/user.routes.js
+++ api/features/users/user.routes.js
@@ -1,18 +1,33 @@
 import express from "express";
-import { registerUser, loginUser, getUserProfile, updateUserProfile } from "./user.controller.js";
-import { protect } from "../../common/middleware/auth.middleware.js";
+import {
+  registerUser,
+  loginUser,
+  getUserProfile,
+  updateUserProfile,
+  getUsers,
+  deleteUser,
+  updateUser,
+} from "./user.controller.js";
+import { authorize, protect } from "../../common/middleware/auth.middleware.js";
 const router = express.Router();
 
 router.post("/register", registerUser);
 router.post("/login", loginUser);
 // Chain Routing
-router.route('/profile')
-.get(protect, getUserProfile)
-.put(protect, updateUserProfile)
+router
+  .route("/profile")
+  .get(protect, getUserProfile)
+  .put(protect, updateUserProfile);
 
+// Admin  only routes
+router.get("/", protect, authorize("admin"), getUsers);
+router.delete("/:id", protect, authorize("admin"), deleteUser);
+router.put("/:id", protect, authorize("admin"), updateUser);
+
+// Test route
 router.get("/test", (req, res) => {
   res.status(200).json({
-    message: "Testing Router",
+    message: "User Endpoint: Router Testing",
   });
 });
 
